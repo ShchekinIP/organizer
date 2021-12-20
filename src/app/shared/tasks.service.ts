4178459ebeeba0dee0d3d10c17420dddbc66844d
ShchekinIP <63049086+ShchekinIP@users.noreply.github.com>
2021-12-20 23:55:57 +0400

    Finish organizer component

---
+++ src/app/shared/tasks.service.ts
@@ -0,0 +1,7 @@
+import {Injectable} from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class TasksService {
+  static url = 'https://ang-calendar-b133b-default-rtdb.firebaseio.com/tasks'
+
+}
