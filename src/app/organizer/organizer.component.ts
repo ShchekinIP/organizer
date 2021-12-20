2489925646d561b178c07cf96701ebb20747a7e3
ShchekinIP <63049086+ShchekinIP@users.noreply.github.com>
2021-12-21 00:27:59 +0400

    Add firebase backend

--- src/app/organizer/organizer.component.ts
+++ src/app/organizer/organizer.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DateService} from "../shared/date.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Task, TasksService} from "../shared/tasks.service";
+import {switchMap} from "rxjs/operators";
 
 @Component({
   selector: 'app-organizer',
@@ -11,18 +13,42 @@ export class OrganizerComponent implements OnInit {
 
   // @ts-ignore
   form: FormGroup
+  tasks: Task[] = []
 
-  constructor(public dateService: DateService) { }
+  constructor(public dateService: DateService,
+              public taskService: TasksService) {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    this.dateService.date.pipe(
+      switchMap(value => this.taskService.load(value))
+    ).subscribe(tasks => {
+      this.tasks = tasks
+    })
     this.form = new FormGroup({
       title: new FormControl('', Validators.required)
     })
   }
 
-  submit(){
+  submit() {
     const {title} = this.form.value
+    const task: Task = {
+      title,
+      date: this.dateService.date.value.format('DD-MM-YYYY')
+    }
+    this.taskService.create(task).subscribe(task => {
+      this.tasks.push(task)
+      this.form.reset()
+
+    }, error => console.error(error))
 
   }
 
+  remove(task: Task) {
+    this.taskService.remove(task).subscribe(() => {
+
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    }, error => console.error(error))
+  }
 }
